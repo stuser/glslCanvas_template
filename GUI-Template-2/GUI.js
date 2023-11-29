0f605c0d5131ca8b417da8447925bbca7dbaf2a1
tc.lin (德全) <stuser.tc@gmail.com>
2023-11-29 13:33:12 +0800

    Update GUI.js

--- GUI-Template-2/GUI.js
+++ GUI-Template-2/GUI.js
@@ -27,7 +27,7 @@ window.onload = function() {
 function preload(){
 	theShader = loadShader('vert.glsl', 'frag.frag');
 	//textureBase = loadImage("data/MonaLisa.jpg");
-	textureBase = loadImage("data/DALL%C2%B7E_sakura_theme_2.png");
+	textureBase = loadImage("data/DALL·E_sakura_theme_2.png");
 }
 // to here
 
